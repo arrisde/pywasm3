420d05d2dfe7acd2593cdbcf3a16813eee45f955
arrisde <maurice@nurfuerspam.de>
2020-03-26 14:40:36 +0100

    fix: cleanup of README, dependencies

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from skbuild import setup
 setup(
     name="pywasm3",
     version="0.1",
-    description="Python bindings for wasm3",
+    description="Python 3 bindings for wasm3",
     author='arrisde',
     license="MIT",
-    packages=['pywasm3']
+    packages=['pywasm3'],
+    install_requires=["cffi==1.14.0"]
 )
